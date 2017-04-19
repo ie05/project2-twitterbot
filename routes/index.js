c565b741631d8ff70e1f797a23218a41fc8a5b29
Handshy <Ian.Handshy@ergodyne.com>
2017-04-18 21:23:05 -0500

    completed refactor pushing heroku

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 	
-	res.render('index', { title: 'I.H. Botman', description: 'Does Something Yet Undecided by Author'});
+	res.render('index', { title: 'Jack Robo Burton', description: 'You know what ol\' Jack Burton always says at a time like this?'});
 
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About', description: 'Does Something Yet Undecided by Author' });
+  res.render('about', { title: 'About', description: 'Ol\' Jack always says... what the hell?' });
 });
 
 module.exports = router;
